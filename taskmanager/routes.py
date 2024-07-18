efe1e3a547a057aee0408deb8488923ad0a3757e
Nicola Getsee <Nicola.getsee@gmail.com>
2024-07-18 20:27:28 +0000

    taskmanager db created and populated

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from taskmanager import app, db
+from taskmanager import app, db
+from taskmanager.models import Category, Task
 
 # This will be used to target a function called 'home', which will just return the rendered_template of "base.html" that we will create shortly.
 @app.route("/")
