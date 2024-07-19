08dd85b045dadcf71991d45fc1ed7ee3c567bce4
Nicola Getsee <Nicola.getsee@gmail.com>
2024-07-19 18:19:43 +0000

    basic setup using nav and content and footer that extends the base class

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -6,4 +6,4 @@ from taskmanager.models import Category, Task
 # This will be used to target a function called 'home', which will just return the rendered_template of "base.html" that we will create shortly.
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("tasks.html")
