b6654e8ca56ca7d8f37a6578f8e1001afd3b7d48
Nicola Getsee <Nicola.getsee@gmail.com>
2024-07-21 09:40:11 +0000

    add tasks part 1

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    //formating the date
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        //we want done to show select
+        i18n: {done: "Select"}
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
 });
